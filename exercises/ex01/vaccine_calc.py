8b619de46a8aff69f28d89a612f2f09897e6572d
Catherine Roberts <crobert7@email.unc.edu>
2021-02-06 17:17:26 -0500

    fixing autograder mistakes pt 1

--- exercises/ex01/vaccine_calc.py
+++ exercises/ex01/vaccine_calc.py
@@ -4,7 +4,6 @@ __author__ = "730439833"
 
 # The datetime data type is imported from the datetime library.
 # A datetime object models a specific date and time.
-#
 # Official Documentation: https://docs.python.org/3/library/datetime.html#datetime-objects
 from datetime import datetime
 
@@ -19,23 +18,26 @@ from datetime import timedelta
 
 # Begin your solution here...
 
-#what we ask the user for
+# What we ask the user for
 population: int = int(input("Population: "))
 doses_administered: int = int(input("Doses administered: "))
 doses_per_day: int = int(input("Doses per day: "))
 target_percent_vaccinated: int = int(input("Target percent vaccinated: "))
 
 
-#definitions for calculation
+# Definitions for calculation
 people_already_vaccinated: float = doses_administered /2
 people_we_want_vaccinated: int = round((target_percent_vaccinated /100) * population)
 people_need_to_vaccinate: float = people_we_want_vaccinated - people_already_vaccinated
 how_many_vaccinate_day: float = doses_per_day /2
-days_it_will_take: int = round(people_need_to_vaccinate / how_many_vaccinate_day) #this is for printing in the string concatenation
-days: timedelta = timedelta(days_it_will_take) #this is for calculating date_will_fall_on using correct format
+days_it_will_take: int = round(people_need_to_vaccinate / how_many_vaccinate_day)  
+# This is for printing in the string concatenation
+days: timedelta = timedelta(days_it_will_take)  
+# This is for calculating date_will_fall_on using correct format
 today: datetime = datetime.today()
 date_will_fall_on: datetime = today + days 
 
 
-#output
-print("We will reach " + str(target_percent_vaccinated) + "% vaccination in " + str(days_it_will_take) + " days which falls on " + str(date_will_fall_on.strftime("%B, %d, %Y")) )
+# Output
+print("We will reach " + str(target_percent_vaccinated) + "% vaccination in " + str(days_it_will_take)
+ + " days, which falls on " + str(date_will_fall_on.strftime("%B, %d, %Y") + ".") )
